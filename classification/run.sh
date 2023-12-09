3ea4ecefe06389b6c89e97dfcdeefbee4edd9086
sherrydoge <787362978@qq.com>
2023-12-10 00:23:58 +0800

    run.sh

--- classification/run.sh
+++ classification/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=2 --master_port=29500 train.py --transform --pretrain
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 --master_port=29500 train.py --transform --pretrain
